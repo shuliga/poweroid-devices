faaef29e8166a3fab5c996766ec90c2805313965
Yuriy Shuliha <yuriy_shuliha@epam.com>
2021-05-03 18:27:11 +0300

    States refactored by templates // Thermo device updated to new states

--- devices/thermo_1-2x2_dht/poweroid_thermo_1-2x2_dht.ino
+++ devices/thermo_1-2x2_dht/poweroid_thermo_1-2x2_dht.ino
@@ -10,6 +10,10 @@
 
 MultiClick btn_override(IN2_PIN);
 
+typedef struct Timings {
+    TimingState floor_switch_delay, heater_switch_delay;
+};
+
 Timings timings = {0, 0};
 
 Context CTX = Context(SIGNATURE, ID, PROPS.FACTORY, PROPS.props_size, PROPS.DEFAULT_PROPERTY);
@@ -60,7 +64,7 @@ void processSensors() {
 }
 
 bool shouldUpdate() {
-    return CTX.propsUpdated || changedState[0] || changedState[1] || current_temp == TEMP_FAIL;
+    return CTX.propsUpdated || stateHolderMode.wasChanged() || stateHolderTempHeater.wasChanged() || current_temp == TEMP_FAIL;
 }
 
 void fillOutput() {
@@ -68,10 +72,11 @@ void fillOutput() {
     strcpy(BANNER.data.text, PWR.SENS->printDht());
 }
 
+
 void run_state_mode(const McEvent _event[]) {
-    switch (state_mode) {
+    switch (stateHolderMode.state) {
         case SM_AWAY: {
-            if (firstStateMode(SM_AWAY)){
+            if (stateHolderMode.firstState(SM_AWAY)) {
                 CTX.PERS.storeState(0, true);
             }
 
@@ -80,35 +85,35 @@ void run_state_mode(const McEvent _event[]) {
 
             if (_event[1] == HOLD) {
                 CTX.PERS.storeState(0, false);
-                gotoStateMode(SM_ECO);
+                stateHolderMode.gotoState(SM_ECO);
             }
             break;
         }
         case SM_ECO: {
-            firstStateMode(SM_ECO);
+            stateHolderMode.firstState(SM_ECO);
 
             floor_temp = PROPS.FACTORY[2].runtime;
             target_heater_temp = PROPS.FACTORY[3].runtime;
 
             if (inverse ? _event[0] == PRESSED : _event[0] == RELEASED) {
-                gotoStateMode(SM_NORMAL);
+                stateHolderMode.gotoState(SM_NORMAL);
             }
             if (_event[1] == HOLD) {
-                gotoStateMode(SM_AWAY);
+                stateHolderMode.gotoState(SM_AWAY);
             }
             break;
         }
         case SM_NORMAL: {
-            firstStateMode(SM_NORMAL);
+            stateHolderMode.firstState(SM_NORMAL);
 
             floor_temp = PROPS.FACTORY[0].runtime;
             target_heater_temp = PROPS.FACTORY[1].runtime;
 
             if (inverse ? _event[0] == RELEASED : _event[0] == PRESSED) {
-                gotoStateMode(SM_ECO);
+                stateHolderMode.gotoState(SM_ECO);
             }
             if (_event[1] == HOLD) {
-                gotoStateMode(SM_AWAY);
+                stateHolderMode.gotoState(SM_AWAY);
             }
             break;
         }
@@ -117,32 +122,32 @@ void run_state_mode(const McEvent _event[]) {
 
 void run_state_temp_heater() {
     bool doHeat = current_temp != TEMP_FAIL && current_temp < target_heater_temp;
-    switch (state_temp_heater) {
+    switch (stateHolderTempHeater.state) {
         case SH_DISARM:{
             firstStateTempHeater(SH_DISARM);
             break;
         }
         case SH_OFF: {
-            firstStateTempHeater(SH_OFF);
-            if (timings.heater_switch_delay.isTimeAfter(doHeat) || (doHeat && (shouldUpdate() || prev_state_temp_heater == SH_DISARM))) {
-                gotoStateTempHeater(SH_HEAT);
+            stateHolderTempHeater.firstState(SH_OFF);
+            if (timings.heater_switch_delay.isTimeAfter(doHeat) || (doHeat && (shouldUpdate() || stateHolderTempHeater.prev_state == SH_DISARM))) {
+                stateHolderTempHeater.gotoState(SH_HEAT);
             }
             break;
         }
         case SH_HEAT: {
-            firstStateTempHeater(SH_HEAT);
+            stateHolderTempHeater.firstState(SH_HEAT);
             if (timings.heater_switch_delay.isTimeAfter(!doHeat) || (shouldUpdate() && !doHeat)) {
-                gotoStateTempHeater(SH_OFF);
+                stateHolderTempHeater.gotoState(SH_OFF);
             }
             break;
         }
     }
 }
 
-
 #ifndef MINI
 void run_state_temp_floor() {
     bool doHeat = current_temp != TEMP_FAIL && current_temp < floor_temp;
+    switch (stateHolderTempFloor.state) {
     switch (state_temp_floor) {
         case SF_DISARM:{
             firstStateTempFloor(SF_DISARM);
@@ -181,18 +186,17 @@ void setup() {
 void loop() {
 
     PWR.run();
-
-    PWR.power(REL_A, state_temp_heater == SH_HEAT);
+    PWR.power(REL_A, stateHolderTempHeater.state == SH_HEAT);
 #ifndef MINI
-    PWR.power(REL_B, state_temp_floor == SF_HEAT);
+    PWR.power(REL_B, stateHolderTempFloor.state == SF_HEAT);
 #endif
 
     if (current_temp == TEMP_FAIL) {
         INDICATORS.flash(IND_1, flash_(timerCounter_4Hz), true);
-    } else if (state_mode == SM_ECO || state_mode == SM_NORMAL) {
-        INDICATORS.set(IND_1, state_mode == SM_ECO);
+    } else if (stateHolderMode.state == SM_ECO || stateHolderMode.state == SM_NORMAL) {
+        INDICATORS.set(IND_1, stateHolderMode.state == SM_ECO);
     } else {
-        INDICATORS.flash(IND_1, !flash_accent(timerCounter_1Hz), state_mode == SM_AWAY);
+        INDICATORS.flash(IND_1, !flash_accent(timerCounter_1Hz), stateHolderMode.state == SM_AWAY);
     }
 
 }
--- devices/thermo_1-2x2_dht/poweroid_thermo_1-2x2_dht_state.h
+++ devices/thermo_1-2x2_dht/poweroid_thermo_1-2x2_dht_state.h
@@ -1,11 +1,7 @@
 #include <commons.h>
 #include <context.h>
-#include <states.h>
 #include <timings.h>
-
-typedef struct Timings {
-    TimingState floor_switch_delay, heater_switch_delay;
-};
+#include <states.h>
 
 enum StateMode {
     SM_AWAY, SM_ECO, SM_NORMAL
@@ -15,27 +11,23 @@ enum StateTempHeater {
     SH_DISARM, SH_OFF, SH_HEAT
 };
 
+#ifndef MINI
 enum StateTempFloor {
     SF_DISARM, SF_OFF, SF_HEAT
-}
-
-#ifndef MINI
-state_temp_floor = SF_OFF, prev_state_temp_floor = SF_DISARM;
-#else
-state_temp_floor = SF_DISARM, prev_state_temp_floor = SF_OFF;
+};
 #endif
-
-const uint8_t state_count = 3;
-bool changedState[state_count] = {false, false, false};
 
 const char *STATE_MODE[] = {"AWAY", "ECO", "NORM"};
 const char *STATE_HEATER[] = {"DISARM", "OFF", "HEAT"};
 
 StateHolder<StateMode> stateHolderMode(SM_AWAY, SM_ECO, SM_AWAY, "Mode", STATE_MODE);
 StateHolder<StateTempHeater> stateHolderTempHeater(SH_DISARM, SH_OFF, SH_DISARM, "Heater t.", STATE_HEATER);
+#ifndef MINI
 StateHolder<StateTempFloor> stateHolderTempFloor(SF_DISARM, SF_OFF, SF_DISARM, "Floor t.", STATE_HEATER);
 
-StateHolderBase* sb = &stateHolderMode;
-
-static const StateHolderBase* run_states[state_count] = {&stateHolderMode, &stateHolderTempHeater, &stateHolderTempFloor};
-
+const uint8_t state_count = 3;
+StateHolderBase* run_states[state_count] = {(StateHolderBase *) &stateHolderMode, (StateHolderBase *) &stateHolderTempHeater, (StateHolderBase *) &stateHolderTempFloor};
+#else
+const uint8_t state_count = 2;
+StateHolderBase* run_states[state_count] = {(StateHolderBase *) &stateHolderMode, (StateHolderBase *) &stateHolderTempHeater};
+#endif
